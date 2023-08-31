7f377115f229187a3cc87b54ef9148e4a23e5969
Yee Y <s0m3y3@gmail.com>
2023-08-30 19:23:08 -0500

    updated server.js. added add.post, but is not complete. commented out referenced code.

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const noteDb = required('./db/db.json');
-// Helper method for generating unique ids
+
 const PORT = 3001;
 const app = express();
 
@@ -23,49 +23,54 @@ app.get('/api/notes', (req, res) => {
   res.json(noteDb);
 });
 
+app.post("/api/notes", (req, res) => {
+
+    fs.writeFile("./db/db.json", JSON.stringify(notes), function (err) {
+        if (err) throw err;
+    })
+})
 
 
 
-
-
+//NOTE: below is code from prior assignment. Only here for an reference.  
 // POST request to add a review
-app.post('/api/reviews', (req, res) => {
-  // Log that a POST request was received
-  console.info(`${req.method} request received to add a review`);
-  // Destructuring assignment for the items in req.body
-  const { product, review, username } = req.body;
-  // If all the required properties are present
-  if (product && review && username) {
-    // Variable for the object we will save
-    const newReview = {
-      product,
-      review,
-      username,
-      upvotes: Math.floor(Math.random() * 100),
-      review_id: uuid(),
-    };
-    // Convert the data to a string so we can save it
-    const reviewString = JSON.stringify(newReview);
-    // Write the string to a file
-    fs.writeFile(`./db/${newReview.product}.json`, reviewString, (err) =>
-      err
-        ? console.error(err)
-        : console.log(
-            `Review for ${newReview.product} has been written to JSON file`
-          )
-    );
+// app.post('/api/reviews', (req, res) => {
+//   // Log that a POST request was received
+//   console.info(`${req.method} request received to add a review`);
+//   // Destructuring assignment for the items in req.body
+//   const { product, review, username } = req.body;
+//   // If all the required properties are present
+//   if (product && review && username) {
+//     // Variable for the object we will save
+//     const newReview = {
+//       product,
+//       review,
+//       username,
+//       upvotes: Math.floor(Math.random() * 100),
+//       review_id: uuid(),
+//     };
+//     // Convert the data to a string so we can save it
+//     const reviewString = JSON.stringify(newReview);
+//     // Write the string to a file
+//     fs.writeFile(`./db/${newReview.product}.json`, reviewString, (err) =>
+//       err
+//         ? console.error(err)
+//         : console.log(
+//             `Review for ${newReview.product} has been written to JSON file`
+//           )
+//     );
 
-    const response = {
-      status: 'success',
-      body: newReview,
-    };
+//     const response = {
+//       status: 'success',
+//       body: newReview,
+//     };
 
-    console.log(response);
-    res.status(201).json(response);
-  } else {
-    res.status(500).json('Error in posting review');
-  }
-});
+//     console.log(response);
+//     res.status(201).json(response);
+//   } else {
+//     res.status(500).json('Error in posting review');
+//   }
+// });
 
 
 
